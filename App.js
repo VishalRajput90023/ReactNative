2eee536b69c1f0a3a4ccd06709c7ed83d591161d
Vishal Rajput <tonyvr53155@gmail.com>
2018-06-15 11:06:19 +0530

    ChangingText

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text } from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <Text>ThinkX</Text>
     );
   }
 }
